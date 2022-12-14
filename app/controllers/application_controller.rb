363e9281f0ede94d9cb9be6291ccfa4b000d190e
mrcsporto <mrcsporto@gmail.com>
2022-12-14 15:48:34 -0300

    add permisiont to only author manage posts attr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :current_user_name
 
   def current_user_name
-    @current_user_name = current_user.email.to_s.split('@')[0]
+    if current_user.present?
+      @current_user_name = current_user.email.to_s.split('@')[0]
+    end
   end
 end
