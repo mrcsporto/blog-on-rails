1bcbc9fe0924a6ac8d442c4424129bd2f494847b
mrcsporto <mrcsporto@gmail.com>
2022-12-16 14:56:08 -0300

    correct translation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to post_url(@post), notice: "Postagem foi criada com sucesso" }
         format.json { render :show, status: :created, location: @post }
       else
         flash.now[:alert] = @post.errors.full_messages.to_sentence
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.html { redirect_to post_url(@post), notice: "Postagem foi atualizada com sucesso" }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: "Postagem foi deletada com sucesso" }
       format.json { head :no_content }
     end
   end
