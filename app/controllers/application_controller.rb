08c3a1aa030614ce52a3f34e48ca0c1958b2ee63
mrcsporto <mrcsporto@gmail.com>
2022-12-17 15:37:43 -0300

    fix method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :current_user_name
 
   def current_user_name
-    current_user.present?
-    @current_user_name = current_user.email.to_s.split('@')[0]
+    if current_user.present?
+      @current_user_name = current_user.email.to_s.split('@')[0]
+    end
   end
 end
