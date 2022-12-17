bca2ff6b8bda6d9a9b70be03eda8a1120e2b4d8f
mrcsporto <mrcsporto@gmail.com>
2022-12-17 15:00:58 -0300

    refactoring method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user_name
 
   def current_user_name
-    if current_user.present?
-      @current_user_name = current_user.email.to_s.split('@')[0]
-    end
+    current_user.present?
+    @current_user_name = current_user.email.to_s.split('@')[0]
   end
 end
